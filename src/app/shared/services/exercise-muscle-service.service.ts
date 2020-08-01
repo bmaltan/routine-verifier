634b4f1001b337c03d3e26142d84d258094f9378
Bugra Murat Altan <bmaltan@live.com>
2020-08-01 14:05:03 +0200

    add exercises

--- src/app/shared/services/exercise-muscle-service.service.ts
+++ src/app/shared/services/exercise-muscle-service.service.ts
@@ -20,7 +20,7 @@ export class ExerciseMuscleService {
 
         { name: 'Gastrocnemius', id: 9, muscleGroup: MuscleGroup.calves },
         { name: 'Soleus', id: 10, muscleGroup: MuscleGroup.calves },
-        { name: 'Tibialis Anterior', id: 11, muscleGroup: MuscleGroup.calves },
+        // { name: 'Tibialis Anterior', id: 11, muscleGroup: MuscleGroup.calves },
         { name: 'Popliteus', id: 12, muscleGroup: MuscleGroup.calves },
 
         { name: 'Pectoralis Major, Sternal', id: 13, muscleGroup: MuscleGroup.chest },
@@ -35,7 +35,7 @@ export class ExerciseMuscleService {
         { name: 'Supinators', id: 21, muscleGroup: MuscleGroup.forearms },
 
         { name: 'Gluteus Maximus', id: 22, muscleGroup: MuscleGroup.hips },
-        { name: 'Abductors', id: 23, muscleGroup: MuscleGroup.hips },
+        // { name: 'Abductors', id: 23, muscleGroup: MuscleGroup.hips },
         { name: 'Flexors', id: 24, muscleGroup: MuscleGroup.hips },
         { name: 'Deep External Rotators', id: 25, muscleGroup: MuscleGroup.hips },
 
@@ -56,7 +56,7 @@ export class ExerciseMuscleService {
         { name: 'Brachialis', id: 37, muscleGroup: MuscleGroup.upperArms },
 
         { name: 'Rectus Abdominis', id: 38, muscleGroup: MuscleGroup.waist },
-        { name: 'Transverse Abdominus', id: 39, muscleGroup: MuscleGroup.waist },
+        // { name: 'Transverse Abdominus', id: 39, muscleGroup: MuscleGroup.waist },
         { name: 'Obliques', id: 40, muscleGroup: MuscleGroup.waist },
         { name: 'Quadratus Lumborum', id: 41, muscleGroup: MuscleGroup.waist },
         { name: 'Erector Spinae', id: 42, muscleGroup: MuscleGroup.waist }
@@ -90,12 +90,12 @@ export class ExerciseMuscleService {
         },
         {
             id: 7, name: 'Bird Dog', primaryMusclesHit: [42], secondaryMusclesHit: [22, 4, 3, 28, 29], isSelected: false, details: {
-                noEquipment: true
+                noEquipment: false
             }
         },
         {
             id: 8, name: 'Standing Calf Raise', primaryMusclesHit: [9], secondaryMusclesHit: [10], isSelected: false, details: {
-                noEquipment: true
+                noEquipment: false
             }
         },
         {
@@ -115,7 +115,7 @@ export class ExerciseMuscleService {
         },
         {
             id: 12, name: 'Crunch', primaryMusclesHit: [38], secondaryMusclesHit: [40], isSelected: false, details: {
-                noEquipment: true
+                noEquipment: false
             }
         },
         {
@@ -123,11 +123,11 @@ export class ExerciseMuscleService {
                 noEquipment: false
             }
         },
-        {
-            id: 15, name: 'Front Lateral Raise', primaryMusclesHit: [31], secondaryMusclesHit: [29, 28, 16, 3, 4], isSelected: false, details: {
-                noEquipment: false
-            }
-        },
+        // {
+        //     id: 15, name: 'Front Lateral Raise', primaryMusclesHit: [31], secondaryMusclesHit: [29, 28, 16, 3, 4], isSelected: false, details: {
+        //         noEquipment: false
+        //     }
+        // },
         {
             id: 16, name: 'Front Neck Bridge', primaryMusclesHit: [26], secondaryMusclesHit: [27, 2, 5, 42], isSelected: false, details: {
                 noEquipment: false
@@ -135,12 +135,12 @@ export class ExerciseMuscleService {
         },
         {
             id: 17, name: 'Good Morning', primaryMusclesHit: [33], secondaryMusclesHit: [22, 34], isSelected: false, details: {
-                noEquipment: true
+                noEquipment: false
             }
         },
         {
             id: 18, name: 'Hip Abduction', primaryMusclesHit: [34], secondaryMusclesHit: [22], isSelected: false, details: {
-                noEquipment: true
+                noEquipment: false
             }
         },
         {
@@ -165,7 +165,7 @@ export class ExerciseMuscleService {
         },
         {
             id: 23, name: 'Kickback', primaryMusclesHit: [35], secondaryMusclesHit: [30, 1, 3, 4, 6], isSelected: false, details: {
-                noEquipment: true
+                noEquipment: false
             }
         },
         {
@@ -190,12 +190,12 @@ export class ExerciseMuscleService {
         },
         {
             id: 29, name: 'Lunge', primaryMusclesHit: [32], secondaryMusclesHit: [22, 34, 10], isSelected: false, details: {
-                noEquipment: true
+                noEquipment: false
             }
         },
         {
             id: 30, name: 'Lying Hip External Rotation', primaryMusclesHit: [25], isSelected: false, details: {
-                noEquipment: true
+                noEquipment: false
             }
         },
         {
@@ -210,32 +210,32 @@ export class ExerciseMuscleService {
         },
         {
             id: 34, name: 'Neck Extension', primaryMusclesHit: [27], secondaryMusclesHit: [2, 5, 26, 42], isSelected: false, details: {
-                noEquipment: true
+                noEquipment: false
             }
         },
         {
             id: 35, name: 'Neck Flexion', primaryMusclesHit: [26], isSelected: false, details: {
-                noEquipment: true
+                noEquipment: false
             }
         },
         {
             id: 36, name: 'Neck Retraction', primaryMusclesHit: [27], secondaryMusclesHit: [2, 5, 42], isSelected: false, details: {
-                noEquipment: true
+                noEquipment: false
             }
         },
         {
             id: 37, name: 'Neck Rotation', primaryMusclesHit: [26], secondaryMusclesHit: [27, 42, 5, 2], isSelected: false, details: {
-                noEquipment: true
+                noEquipment: false
             }
         },
         {
             id: 38, name: 'Pike Press', primaryMusclesHit: [28], secondaryMusclesHit: [14, 35, 29, 3, 4, 16], isSelected: false, details: {
-                noEquipment: true
+                noEquipment: false
             }
         },
         {
             id: 39, name: 'Pronation', primaryMusclesHit: [20], isSelected: false, details: {
-                noEquipment: true
+                noEquipment: false
             }
         },
         {
@@ -295,7 +295,7 @@ export class ExerciseMuscleService {
         },
         {
             id: 52, name: 'Seated Twist', primaryMusclesHit: [40], isSelected: false, details: {
-                noEquipment: true
+                noEquipment: false
             }
         },
         {
@@ -335,12 +335,12 @@ export class ExerciseMuscleService {
         },
         {
             id: 61, name: 'V-up', primaryMusclesHit: [38], secondaryMusclesHit: [24, 34], isSelected: false, details: {
-                noEquipment: true
+                noEquipment: false
             }
         },
         {
             id: 70, name: 'Wall Side Neck Bridge', primaryMusclesHit: [26], secondaryMusclesHit: [27, 2, 5, 42], isSelected: false, details: {
-                noEquipment: true
+                noEquipment: false
             }
         },
         {
@@ -367,7 +367,283 @@ export class ExerciseMuscleService {
             id: 69, name: 'Y Raise', primaryMusclesHit: [29], secondaryMusclesHit: [31, 7, 3, 4, 16, 28, 30], isSelected: false, details: {
                 noEquipment: false
             }
-        }
+        },
+        {
+            id: 70, name: 'Standing Cross Row', primaryMusclesHit: [30], secondaryMusclesHit: [7, 29, 3, 4, 6, 37, 17], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 71, name: 'Dumbbell Lying Rear Delt Row', primaryMusclesHit: [30], secondaryMusclesHit: [7, 29, 3, 4, 6, 37, 17], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 72, name: 'Dumbbell Lying Rear Lateral Raise', primaryMusclesHit: [30], secondaryMusclesHit: [7, 29, 3, 4, 6], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 73, name: 'Handstand Shoulder Press', primaryMusclesHit: [28], secondaryMusclesHit: [14, 35, 29, 3, 4], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 74, name: 'Triceps Dip', primaryMusclesHit: [35], secondaryMusclesHit: [28, 13, 14, 15, 6, 5], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 75, name: 'JM Bench Press', primaryMusclesHit: [35], secondaryMusclesHit: [28, 13, 14], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 76, name: 'Lying Triceps Extension', primaryMusclesHit: [35], secondaryMusclesHit: [], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 77, name: 'Skullcrusher', primaryMusclesHit: [35], secondaryMusclesHit: [], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 78, name: 'Decline Triceps Extension', primaryMusclesHit: [35], secondaryMusclesHit: [], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 79, name: 'Incline Triceps Extension', primaryMusclesHit: [35], secondaryMusclesHit: [], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 80, name: 'Bent-over Triceps Extension', primaryMusclesHit: [35], secondaryMusclesHit: [], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 81, name: 'Forward Triceps Extension', primaryMusclesHit: [35], secondaryMusclesHit: [], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 82, name: 'Kneeling Triceps Extension', primaryMusclesHit: [35], secondaryMusclesHit: [], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 83, name: 'Incline Pushdown', primaryMusclesHit: [35], secondaryMusclesHit: [], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 84, name: 'Lying Triceps Extension', primaryMusclesHit: [35], secondaryMusclesHit: [], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 85, name: 'Preacher Curl', primaryMusclesHit: [37], secondaryMusclesHit: [36, 17], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 86, name: 'Prone Incline Curl', primaryMusclesHit: [37], secondaryMusclesHit: [36, 17], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 87, name: 'Reverse Preacher Curl', primaryMusclesHit: [37], secondaryMusclesHit: [36, 17], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 88, name: 'Hammer Curl', primaryMusclesHit: [17], secondaryMusclesHit: [36, 37], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 89, name: 'Hammer Preacher Curl', primaryMusclesHit: [17], secondaryMusclesHit: [36, 37], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 90, name: 'Lever Grip', primaryMusclesHit: [18], secondaryMusclesHit: [], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 91, name: 'Sled One Hand Grip', primaryMusclesHit: [18], secondaryMusclesHit: [], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 92, name: 'Underhand Inverted Row', primaryMusclesHit: [1, 2, 3, 4, 5, 6, 7, 8], secondaryMusclesHit: [3, 4, 6, 1, 30, 7, 37, 17, 13], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 93, name: 'Suspended Power Pull', primaryMusclesHit: [1, 2, 3, 4, 5, 6, 7, 8], secondaryMusclesHit: [3, 4, 6, 1, 30, 7, 37, 17, 13, 14, 40, 41], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 94, name: 'Decline Fly', primaryMusclesHit: [13], secondaryMusclesHit: [14, 28, 36], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 95, name: 'Lying Fly', primaryMusclesHit: [13], secondaryMusclesHit: [14, 28, 36], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 95, name: 'Seated Fly', primaryMusclesHit: [13], secondaryMusclesHit: [14, 28, 36], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 96, name: 'Standing Fly', primaryMusclesHit: [13], secondaryMusclesHit: [14, 15, 6, 5, 1], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 97, name: 'Decline Chest Press', primaryMusclesHit: [13], secondaryMusclesHit: [14, 28, 35], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 98, name: 'Decline Bench Press', primaryMusclesHit: [13], secondaryMusclesHit: [14, 28, 35], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 99, name: 'Board Press', primaryMusclesHit: [13], secondaryMusclesHit: [14, 28, 35], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 100, name: 'Floor Press', primaryMusclesHit: [13], secondaryMusclesHit: [14, 28, 35], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 100, name: 'Guillotine Bench Press', primaryMusclesHit: [13], secondaryMusclesHit: [14, 28, 35], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 101, name: 'Dumbbell Fly', primaryMusclesHit: [13], secondaryMusclesHit: [14, 28, 36], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 102, name: 'Pec Deck Fly', primaryMusclesHit: [13], secondaryMusclesHit: [14, 15, 16], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 103, name: 'Seated Iron Cross Fly', primaryMusclesHit: [13], secondaryMusclesHit: [14, 15, 6, 5, 1], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 104, name: 'Push Crunch', primaryMusclesHit: [38], secondaryMusclesHit: [40], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 105, name: 'Push Sit-up', primaryMusclesHit: [38], secondaryMusclesHit: [40], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 106, name: 'Kneeling Crunch', primaryMusclesHit: [38], secondaryMusclesHit: [40], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 107, name: 'Standing Crunch', primaryMusclesHit: [38], secondaryMusclesHit: [40], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 108, name: 'Lying Leg-Hip Raise', primaryMusclesHit: [38], secondaryMusclesHit: [40], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 109, name: 'Suspended Rollout', primaryMusclesHit: [38], secondaryMusclesHit: [1, 13, 15, 30, 6], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 110, name: 'Suspended Jack-knife', primaryMusclesHit: [38], secondaryMusclesHit: [40], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 111, name: 'Suspended Jack-knife Pike', primaryMusclesHit: [38], secondaryMusclesHit: [40], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 112, name: 'Suspended Pull Through', primaryMusclesHit: [38], secondaryMusclesHit: [40], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 113, name: 'Front Plank', primaryMusclesHit: [38], secondaryMusclesHit: [40, 32, 16], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 114, name: 'Step-up', primaryMusclesHit: [32], secondaryMusclesHit: [22, 10], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 115, name: 'Front Squat', primaryMusclesHit: [32], secondaryMusclesHit: [22, 10], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 116, name: 'Hack Squat', primaryMusclesHit: [32], secondaryMusclesHit: [22, 10], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 117, name: 'Rear Lunge', primaryMusclesHit: [32], secondaryMusclesHit: [22, 10], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 118, name: 'Split Squat', primaryMusclesHit: [32], secondaryMusclesHit: [22, 10], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 119, name: 'Seated Leg Press', primaryMusclesHit: [22], secondaryMusclesHit: [32, 10], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 120, name: 'Vertical Leg Press', primaryMusclesHit: [32], secondaryMusclesHit: [22, 10], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 121, name: 'Safety Calf Raise', primaryMusclesHit: [9], secondaryMusclesHit: [10], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+        {
+            id: 122, name: 'Donkey Calf Raise', primaryMusclesHit: [9], secondaryMusclesHit: [10], isSelected: false, details: {
+                noEquipment: false
+            }
+        },
+
     ]
 
     private selectedMuscle: BehaviorSubject<string> = new BehaviorSubject('');
@@ -381,6 +657,15 @@ export class ExerciseMuscleService {
     private isEvaluating: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
     constructor() {
+        this.exercises = this.exercises.sort((a, b) => {
+            let comparison = 0;
+            if (a.name.toLowerCase() > b.name.toLowerCase()) {
+                comparison = 1;
+            } else if (b.name.toLowerCase() > a.name.toLowerCase()) {
+                comparison = -1;
+            }
+            return comparison;
+        })
     }
 
     getExercises() {
